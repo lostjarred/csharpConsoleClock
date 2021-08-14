cc2cf10b0bfa5567780c1eaa48383c24da23329b
lostjarred <lostone1993@hotmail.com>
2021-08-14 13:11:59 +1000

    add project files

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            while (true) {
+                Console.SetCursorPosition(0, 0);
+                var datetimenow = System.DateTime.Now;
+                Console.WriteLine("Greetings user,");
+                Console.WriteLine($"The time is {datetimenow.Hour}:{datetimenow.Minute}:{datetimenow.Second} On {datetimenow.Day}.{datetimenow.Month}.{datetimenow.Year}");
+                Console.WriteLine($"The time is {binstring(datetimenow.Hour)}:{binstring(datetimenow.Minute)}:{binstring(datetimenow.Second)} On {binstring(datetimenow.Day)}.{binstring(datetimenow.Month)}.{binstring(datetimenow.Year)}");
+                Console.WriteLine($"The time is {hexstring(datetimenow.Hour)}:{hexstring(datetimenow.Minute)}:{hexstring(datetimenow.Second)} On {hexstring(datetimenow.Day)}.{hexstring(datetimenow.Month)}.{hexstring(datetimenow.Year)}");
+            }
+        }
+
+        static string binstring(int input)
+        {
+            var output = Convert.ToString(input, 2);
+            return output;
+        }
+
+        static string hexstring(int input)
+        {
+            var output = Convert.ToString(input, 16);
+            return output;
+        }
+    }
+}
